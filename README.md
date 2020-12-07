a8e7372bceb0465913ee0379f00409a13af48696
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2020-12-07 11:33:17 +0100

    basic routing

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
 # Go server
 
-## Table of Contents
-
 - [About](#about)
 
 ## About <a name = "about"></a>
 
-## Usage <a name = "usage"></a>
-
-Add notes about how to use the system.
+Simple server using _Go_.
--- main.go
+++ main.go
@@ -1,86 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-// Planet struct
-type Planet struct {
-	Name       string `json:"name"`
-	Population string `json:"population"`
-	Terrain    string `json:"terrain"`
-	Gravity    string `json:"gravity"`
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Home Page")
 }
-
-// Person struct
-type Person struct {
-	Name         string `json:"name"`
-	HomeWorldURL string `json:"homeworld"`
-	Gender       string `json:"gender"`
-	HomePlanet   Planet
-}
-
-// AllPeople struct
-type AllPeople struct {
-	People []Person `json:"results"`
-}
-
-// BaseURL constant
-const BaseURL = "https://www.swapi.tech/api/"
-
-func (p *Person) getHomeWorld() {
-	res, err := http.Get(p.HomeWorldURL)
-	if err != nil {
-		log.Print("Failed to fetch home world", err)
-	}
-
-	var bytes []byte
-
-	if bytes, err = ioutil.ReadAll(res.Body); err != nil {
-		log.Print("error reading planet request", err)
-	}
-
-	json.Unmarshal(bytes, &p.HomePlanet)
-
-}
-
-func getPeople(w http.ResponseWriter, r *http.Request) {
-	res, err := http.Get(BaseURL + "people")
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		log.Print("Failed to request people")
-	}
-
-	bytes, err := ioutil.ReadAll(res.Body)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		log.Print("failed to parse request body")
-	}
-
-	var people AllPeople
-
-	// similar to json.parse in javascript
-	// give me the memory address where we want to store this information
-	if err := json.Unmarshal(bytes, &people); err != nil {
-		log.Print("Error parsing json", err)
-	}
-
-	fmt.Println(people)
-
-	for _, p := range people.People {
-		// p.getHomeWorld()
-		fmt.Println(p)
-	}
+func TaskManager(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Task manager")
 }
 
 func main() {
-	http.HandleFunc("/", getPeople)
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/tasks", TaskManager)
+
 	fmt.Println("server is on port: 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
